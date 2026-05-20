58651792b323f666ac2acea83b7b9027aa3768d2
Juan Diego Gómez Guzmán <122643460+JuanPyC@users.noreply.github.com>
2026-05-20 03:47:28 +0000

    feat(api): setup express server with error handling and database connection

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,12 +1,26 @@
-import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { PrismaClient } from '../../generated';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
 let prisma: PrismaClient;
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL no está definida');
+}
+
+const adapter = new PrismaPg({ connectionString });
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({ adapter });
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({ adapter });
   }
   prisma = global.prisma;
 }
